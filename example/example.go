73cce972a33313d02b82f523e4e9a436a41d1533
Yma Het <yma.het@gmail.com>
2025-08-29 16:38:28 +0600

    chore: Replace ubuntu kersion of libkqueue with self-built

--- example/example.go
+++ example/example.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/yma-het/waitpid-go"
 )
 
 func main() {
+	pidStr := os.Args[1]
+	pid, err := strconv.Atoi(pidStr)
+	if err != nil {
+		fmt.Println("strconv.Atoi: %v", err)
+		return
+	}
 	w := waitpid.GetWaitHandle()
 	// windows
 	//fd, err := w.Open(0x114)
-	fd, err := w.Open(48)
+	fd, err := w.Open(pid)
 	if err != nil {
 		fmt.Println("w.Open: %v", err)
 		return
