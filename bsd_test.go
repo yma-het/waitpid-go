c998204279add270f47b607561bbd06c974f67a9
l2k1 <yma.het@gmail.com>
2025-09-02 00:26:42 +0600

    wip: Parsed combined stream with stdcopy

--- bsd_test.go
+++ bsd_test.go
@@ -1,41 +1,38 @@
 package waitpid
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"path/filepath"
 	"runtime"
 	"strconv"
 	"testing"
 
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/testcontainers/testcontainers-go"
 	tcexec "github.com/testcontainers/testcontainers-go/exec"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func ProcessContainerExecResult(exitCode int, r io.Reader) (string, error) {
-	out, err := io.ReadAll(r)
-	if err != nil {
-		return "", fmt.Errorf("failed to read from container otput: %w", err)
-	}
-	if exitCode != 0 {
-		return "", fmt.Errorf("got non-zero exit code %d: %s", exitCode, string(out))
-	}
-	return string(out), nil
-}
-
 func ExecGetLog(ctx context.Context, t *testing.T, container testcontainers.Container, cmd []string, options ...tcexec.ProcessOption) (string, error) {
 	t.Logf("will run: %s", cmd)
-	i, r, err := container.Exec(ctx, cmd, options...)
+	processOption := tcexec.WithEnv([]string{"LS_COLORS=\"\""})
+	options = append(options, processOption)
+	exitCode, r, err := container.Exec(ctx, cmd, options...)
 	if err != nil {
 		return "", fmt.Errorf("failed to exec: %w", err)
 	}
-	result, err := ProcessContainerExecResult(i, r)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	_, err = stdcopy.StdCopy(&stdout, &stderr, r)
 	if err != nil {
-		return "", err
+		t.Fatalf("failed to demultiplex output: %v", err)
 	}
-	return result, nil
+	if exitCode != 0 {
+		return "", fmt.Errorf("got non-zero exit code %d\nstdout: %s\nstderr: %s", exitCode, stdout.String(), stderr.String())
+	}
+	return stdout.String(), nil
 }
 
 func InstallGolang(ctx context.Context, t *testing.T, c testcontainers.Container) error {
@@ -48,23 +45,21 @@ func InstallGolang(ctx context.Context, t *testing.T, c testcontainers.Container
 	}
 	//t.Logf("go fetch log: %s", log)
 	//_, _, err = c.Exec(ctx, []string{"tar", "-C", "/usr/local", "-xzf", "/tmp/go.tar.gz"})
-	_, err = ExecGetLog(ctx, t, c, []string{"tar", "-C", "/usr/local", "-xzf", "/tmp/go.tar.gz"})
+	log, err := ExecGetLog(ctx, t, c, []string{"tar", "-C", "/usr/local", "-xzf", "/tmp/go.tar.gz"})
 	if err != nil {
 		return fmt.Errorf("failed to extract golang: %v", err)
 	}
-	//t.Logf("go archive extraction log: %s", log)
+	t.Logf("go archive extraction log: %s", log)
 	//_, _, err = c.Exec(ctx, []string{"rm", "/tmp/go.tar.gz"})
 	_, err = ExecGetLog(ctx, t, c, []string{"rm", "/tmp/go.tar.gz"})
 	if err != nil {
 		return fmt.Errorf("failed to cleanup golang binary: %v", err)
 	}
-	//_, _, err = c.Exec(ctx, []string{"ln", "-s", "/usr/local/go/bin/go", "/usr/bin/go"})
 	_, err = ExecGetLog(ctx, t, c, []string{"ln", "-sf", "/usr/local/go/bin/go", "/usr/bin/go"})
 	if err != nil {
 		return fmt.Errorf("failed to create symlink to golang binary: %v", err)
 	}
 	_, err = ExecGetLog(ctx, t, c, []string{"ln", "-sf", "/usr/local/go/bin/gofmt", "/usr/bin/gofmt"})
-	//_, _, err = c.Exec(ctx, []string{"ln", "-s", "/usr/local/go/bin/gofmt", "/usr/bin/gofmt"})
 	if err != nil {
 		return fmt.Errorf("failed to create symlink to gofmt binary: %v", err)
 	}
@@ -102,12 +97,12 @@ func TestHelloEmpty(t *testing.T) {
 	}
 
 	// this will spawn sleep process and print it's pid to stdout
-	log, err := ExecGetLog(ctx, t, container, []string{"/bin/bash", "-c", "(sleep 10000 &); echo $!"})
+	log, err := ExecGetLog(ctx, t, container, []string{"/bin/bash", "-c", "sleep 10000 & echo $!"})
 	if err != nil {
 		t.Fatalf("failed to spawn sleep process: %v", err)
 	}
 
-	_, err = strconv.Atoi(log)
+	_, err = strconv.Atoi(log[:len(log)-1])
 	if err != nil {
 		t.Fatalf("failed to convert container output to pid from (%s): %v", log, err)
 	}
